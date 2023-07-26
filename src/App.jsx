cf78a85149b840dc8c99a7b951a4ae2e8d51efe2
AJ <mahidlawonaljun@gmail.com>
2023-07-26 10:59:22 +0800

    Add roll button to generate random dice numbers

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,19 @@ function App() {
     return arrNum;
   }
 
+  function rollDice() {
+    setAllNewDice(dieNumbers);
+  }
+
   const dice = allNewDice.map((die, index) => <Die key={index} value={die} />);
+  console.log(dice);
 
   return (
     <main>
       <div className='dice-container'>{dice}</div>
+      <button type='button' onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
