27a342e4f997e4b27fa89902e45481395ef29618
AJ <mahidlawonaljun@gmail.com>
2023-07-28 23:00:18 +0800

    Update path

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,4 +1,4 @@
-import { BsDice1, BsDice2, BsDice3, BsDice4, BsDice5, BsDice6 } from 'react-icons/Bs';
+import { BsDice1, BsDice2, BsDice3, BsDice4, BsDice5, BsDice6 } from '/node_modules/react-icons/bs';
 import './Die.css';
 
 export default function Die({ value, isHeld, holdDice }) {
